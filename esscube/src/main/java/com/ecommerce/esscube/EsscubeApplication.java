25185c0ca4fe6bcb1654a4db7556d064cee80f1b
JaySanSen <183524427+JaySanSen@users.noreply.github.com>
2025-10-04 17:26:49 +0530

    create: initial commit backend project

---
+++ esscube/src/main/java/com/ecommerce/esscube/EsscubeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.esscube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsscubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsscubeApplication.class, args);
+	}
+
+}
---
+++ esscube/src/test/java/com/ecommerce/esscube/EsscubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.esscube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsscubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
