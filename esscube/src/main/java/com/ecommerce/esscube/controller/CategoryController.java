0a528374474fb0842cb61a92f3420f60d097a089
JaySanSen <183524427+JaySanSen@users.noreply.github.com>
2025-10-05 04:32:08 +0530

    some additional ways to add response entity
    Signed-off-by: JaySanSen <183524427+JaySanSen@users.noreply.github.com>

--- esscube/src/main/java/com/ecommerce/esscube/controller/CategoryController.java
+++ esscube/src/main/java/com/ecommerce/esscube/controller/CategoryController.java
@@ -25,14 +25,14 @@ public class CategoryController {
   }
 
   @GetMapping("api/public/categories")
-  public List<Category> getAllCategories() {
-    return categoryService.getAllCategories();
+  public ResponseEntity<List<Category>> getAllCategories() {
+    return ResponseEntity.status(HttpStatus.OK).body(categoryService.getAllCategories());
   }
 
   @PostMapping("api/public/categories")
-  public String createCategory(@RequestBody Category category) {
+  public ResponseEntity<String> createCategory(@RequestBody Category category) {
     categoryService.createCategory(category);
-    return "Category added successfully";
+    return ResponseEntity.ok("Category created");
   }
 
   @DeleteMapping("api/admin/categories/{categoryId}")
